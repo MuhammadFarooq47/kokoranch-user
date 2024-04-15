c112b2825d5d29eb5b7e002a50b5f35d71334725
MuhammadFarooq47 <m.farooq.h7@gmail.com>
2024-04-15 12:31:50 +0500

    add vercel link

--- src/apis/constant.js
+++ src/apis/constant.js
@@ -1,7 +1,8 @@
 const LIVE_URL = "https://kokoranch-backend-45665121adb2.herokuapp.com/api/v1/";
 // const LIVE_URL = "https://52.66.63.79/api/v1";
 // const LOCAL_URL = "https://kokoranch-backend-45665121adb2.herokuapp.com/api/v1/";
-const LOCAL_URL = "https://kokoranch-backend-45665121adb2.herokuapp.com/api/v1";
+// const LOCAL_URL = "https://kokoranch-backend-45665121adb2.herokuapp.com/api/v1";
+const LOCAL_URL = "https://kokoranch-backend.vercel.app/api/v1";
 // const LIVE_URL = "https://192.168.100.33:3030/api/v1";
 
 // const BASE_URL = LIVE_URL;
